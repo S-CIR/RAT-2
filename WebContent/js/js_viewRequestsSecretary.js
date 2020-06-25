5f19218981912bed890d7363a7d28686682be60a
Lorenzo Galasso <lorenzogalass099@gmail.com>
2020-06-25 11:39:51 +0200

    aggiunte jsp di segreteria e admin, minor fixes

---
+++ WebContent/js/js_viewRequestsSecretary.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+});
+
+function showData() {
+	$(".preloader").show();
+
+	$.ajax({
+		url : absolutePath + "/ServletSecretary",
+		type : "POST",
+		dataType : 'JSON',
+		async : false,
+		data : {
+			"flag" : 1
+		},
+		success : function(msg) {
+			if (!msg.result) {
+				showAlert(1, msg.error);
+			} else {
+				$("#bodySegretaryBody").html(msg.content);
+			}
+		},
+		error : function(msg) {
+			showAlert(1, "Impossibile Recuperare i dati.");
+		}
+	});
+
+	$(".preloader").hide();
+}
