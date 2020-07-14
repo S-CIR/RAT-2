45fb7aa86716f4b9e9385da7ef67459c6acce2ff
Lorenzo Galasso <lorenzogalass099@gmail.com>
2020-07-14 18:31:32 +0200

    Aggiunti test di Admin, Secretary, Request e Azienda

--- Java Resources/src/test/Test.java
+++ Java Resources/src/test/Test.java
@@ -3,17 +3,24 @@ package src.test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import src.controller.DBConnection;
 import src.interfaccia.UserInterface;
-import src.model.Student;
-import src.model.UtenteDAO;
+import src.model.Attached;
+import src.model.Request;
 
 public class Test {
 
 	public static void main(String[] args) {
-		UserInterface user = new Student("a", "a", 'M', "c","a", 0);
-		UtenteDAO.doSave(user);
+		List<Attached> lst = new ArrayList<>();
+		Request re = new Request (01, 60, 6, new Date(), new Date(), 0, lst, 1, 1, "prova@studenti.unisa.it");
+		
+		System.out.println(re.toString());
+		
+		
 	}
 
 }
